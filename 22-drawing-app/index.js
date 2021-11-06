86c2fd5dff9b3c67e56ce82def7bdc5127ffeea3
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-11-06 18:33:10 -0500

    22 | Drawing App

---
+++ 22-drawing-app/index.js
@@ -0,0 +1,78 @@
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+let size = 10, color = 'black', isPressed = false, x, y
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+  x = e.offsetX
+  y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false
+  x = undefined
+  y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2)
+
+    x = x2
+    y = y2
+  }
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
+
+increaseBtn.addEventListener('click', () => {
+  size += 5
+
+  if (size > 50) {
+    size = 50
+  }
+
+  updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5
+
+  if (size < 5) {
+    size = 5
+  }
+
+  updateSizeOnScreen()
+})
+
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size
+}
+
+function drawCircle(x, y) {
+  ctx.beginPath()
+  ctx.arc(x, y, size, 0, Math.PI * 2, true)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = color
+  ctx.lineWidth = size * 2
+  ctx.stroke()
+}
